d077d0bc1d2949f2866618ecc9249846f63d2ec5
YehorPorubaimikh <porubaimikh.yehor@gmail.com>
2020-01-21 23:03:43 +0200

    groupList

---
+++ src/components/GroupsList.js
@@ -0,0 +1,29 @@
+import { FirebaseContext } from "../context/firebase/firebaseContext"
+import React, { useContext } from "react"
+
+export const GroupsList = () => {
+  const { groupsList } = useContext(FirebaseContext)
+
+  // const groupsList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  //   .map(i => { return { id: 'id' + i, name: 'group' + i, lastMessage: 'msg' + i } })
+
+
+  const selectGroup = (id) => {
+    console.log(id);
+  }
+
+
+  return (
+    <div className="group-list">
+      {
+        groupsList.map(i => {
+          return (
+            <p key={i.id} onClick={() => selectGroup(i.id)}>{i.name}</p>
+          )
+        })
+      }
+    </div>
+  )
+
+
+}
