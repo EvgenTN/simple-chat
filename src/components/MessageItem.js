32a7958fb9423c98f0eb279d2c34884f5d163161
evgentn <evgentitovn@gmail.com>
2020-01-29 21:15:50 +0200

    add card

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -1,18 +1,21 @@
 import React from 'react'
-// import style from './MessageItem.module.scss'
 
-export const MessageItem = ({message}) => {
+export const MessageItem = ({ message }) => {
 
   const formatedDate = () => {
     return new Date(message.createdAt.seconds * 1000).toISOString().slice(0, -8).replace('T', ' ')
   }
 
   return (
-    <div >
-      <h5>{message.userName}</h5>
-      <p>{message.text}</p>
+    <div className="relative bg-white rounded border p-4 m-4 max-w-md">
+      <h3 className="flex justify-between items-center text-lg mb-2 font-bold">
+        <p className="stretched-link" title="Card 6">
+          {message.userName}
+        </p>
+        <p className="text-sm text-gray-600">{formatedDate()}</p>
+      </h3>
       <p>
-        <small>{formatedDate()}</small>
+        {message.text}
       </p>
     </div>
   )
